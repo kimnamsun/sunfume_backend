10399fda855c804ef087c0ca8ee821b19bbc52de
kimnamsun <nsunny0908@naver.com>
2021-05-23 14:15:47 +0900

    Add :디테일 페이지 상품리뷰조회 기능

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,26 @@
 class ReviewsController < ApiController
   before_action :set_user, only: [:index]
+  before_action :set_item, only: [:show]
 
   def index
     item_id = @user.reviewed_items
     review_items = Item.where(id: item_id)
     render json: each_serialize(review_items)
   end
+
+  def show
+    user_id = @item.reviewed_user
+    user = User.where(id: user_id)
+    render json: each_serialize(user)
+  end
   
   private
   def set_user
     @user = current_api_user
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
--- app/serializers/user_each_serializer.rb
+++ app/serializers/user_each_serializer.rb
@@ -2,4 +2,5 @@
     include ImagableSerializer
 
     attributes :id, :email, :name, :description, :follow_id
+    has_many :reviews, serializer: ReviewEachSerializer
   end
